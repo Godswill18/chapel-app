7983891d7dcc657c6dd614cd60fdee086d8d443d
Godswill Okhamera <gokhamera@gmail.com>
2025-09-24 19:41:23 +0100

    url adjustmet

--- chapel_app/src/contexts/AuthContext.js
+++ chapel_app/src/contexts/AuthContext.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand';
 import { persist } from 'zustand/middleware';
 
-const API_URL = import.meta.env.VITE_BACKEND_API_URL || 'https://wsu-chapel.onrender.com/'
+const API_URL = import.meta.env.VITE_BACKEND_API_URL || 'https://chapel-app.vercel.app/'
 
  export const useUserContext = create((set) => ({
     userContext: [],
